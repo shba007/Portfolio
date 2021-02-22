6b303c36ff26914f955cd5dd8ee3710337e65b92
Shirsendu Bairagi <shirsendu2001@gmail.com>
2021-02-22 14:41:48 +0530

    section component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, SectionComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
