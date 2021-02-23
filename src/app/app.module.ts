ecec9bfb9ad372cec9f3fe5164cd7ec983be83a1
Shirsendu Bairagi <shirsendu2001@gmail.com>
2021-02-23 10:29:56 +0530

    hero component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SectionComponent } from './section/section.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SectionComponent],
+  declarations: [AppComponent, HeaderComponent, SectionComponent, HeroComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
