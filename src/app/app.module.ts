e26bffe0530b60f41c2f66c374ab41a425c7e4c7
Shirsendu Bairagi <shirsendu2001@gmail.com>
2021-02-26 00:26:57 +0530

    section component deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { SectionComponent } from './section/section.component';
 import { HeroComponent } from './hero/hero.component';
 import { CardComponent } from './card/card.component';
 import { HexagonComponent } from './hexagon/hexagon.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SectionComponent, HeroComponent, CardComponent, HexagonComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    CardComponent,
+    HexagonComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
