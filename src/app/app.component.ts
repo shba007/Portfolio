f2b5028fc29af977199230efed3f7fb0b8df28c5
Shirsendu Bairagi <shirsendu2001@gmail.com>
2021-02-26 15:44:27 +0530

    hexagon, section refactored

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,19 @@ import { Component } from '@angular/core';
 import projects from './database/projects.json';
 import technologies from './database/technologies.json';
 
+import { Project } from './models/project.model';
+import { Technology } from './models/technologies.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'portfolio';
-  projects = projects;
-  technologies = technologies;
+  projects: Project[] = projects;
+  technologies: Technology[] = technologies;
+
+  getAlign({ x, y }) {
+    return { 'grid-row': `${y} / span 3`, 'grid-column': `${x} / span 2` };
+  }
 }
