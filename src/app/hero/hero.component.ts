08900c308a23b45c25c93daac51f5f0283a5fe9e
Shirsendu Bairagi <shirsendu2001@gmail.com>
2021-02-28 18:49:02 +0530

    hero section refactored

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import Typewriter from 'typewriter-effect/dist/core';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss']
+  styleUrls: ['./hero.component.scss'],
 })
-export class HeroComponent implements OnInit {
+export class HeroComponent implements OnInit, AfterViewInit {
+  @ViewChild('textContent') textContent: ElementRef;
 
-  constructor() { }
+  constructor() {}
+  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    const textElem = this.textContent.nativeElement;
+    const typewriter = new Typewriter(textElem);
 
-  ngOnInit(): void {
+    typewriter
+      .pauseFor(2500)
+      .typeString('Following the <br>"Logical Sequential Steps" <br>to avoid ')
+      .pauseFor(50)
+      .typeString(`Failure`)
+      .pauseFor(300)
+      .deleteChars(7)
+      .pauseFor(500)
+      .typeString(`Error`)
+      .pauseFor(250)
+      .deleteChars(5)
+      .pauseFor(1500)
+      .typeString(`<span class="highlight">Failure+Error</span>`)
+      .callFunction(() => {
+        textElem.querySelector('.highlight').textContent = 'FailError';
+      })
+      .start();
   }
-
 }
