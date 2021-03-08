c1780cf31d0611d97ee69c8e8d6248ebbcaf64cd
Shirsendu Bairagi <shirsendu2001@gmail.com>
2021-03-08 15:10:30 +0530

    contact email functional

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,18 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import emailjs from 'emailjs-com';
+import apiKeys from '../others/apiKeys';
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
-  constructor() {}
+  visible = false;
 
+  constructor() {}
   ngOnInit(): void {}
 
   onSubmit(form: NgForm) {
-    const value = form.value;
-    console.log(value);
+    // Send to Dev email
+    emailjs
+      .send(
+        apiKeys.SERVICE_ID,
+        apiKeys.TEMPLATE_ID.dev,
+        form.value,
+        apiKeys.USER_ID
+      )
+      .then((result) => {
+        console.log(result.text);
+      })
+      .catch((error) => {
+        console.log(error.text);
+      });
+    // Send to User email
+    emailjs
+      .send(
+        apiKeys.SERVICE_ID,
+        apiKeys.TEMPLATE_ID.user,
+        form.value,
+        apiKeys.USER_ID
+      )
+      .then((result) => {
+        console.log(result.text);
+      })
+      .catch((error) => {
+        console.log(error.text);
+      });
+
+    this.changeStatus();
+  }
+  changeStatus() {
+    this.visible = true;
+    setTimeout(() => this.onClose(), 5000);
+  }
+  onClose() {
+    this.visible = false;
   }
 }
