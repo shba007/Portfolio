3f7fc330a1cd69a1d611b6670c249ab2518ddb0c
Shirsendu Bairagi <shirsendu2001@gmail.com>
2021-03-22 23:32:14 +0530

    npm typewriter-effect added

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import Typewriter from './Typewriter';
+import Typewriter from 'typewriter-effect/dist/core';
 
 @Component({
   selector: 'app-hero',
@@ -15,23 +15,23 @@ export class HeroComponent implements AfterViewInit {
     const typewriter = new Typewriter(textElem);
 
     typewriter
-      .pause(2500)
-      .type('Following the ')
-      .markup('<br>')
-      .type('"Logical Sequential Steps" ')
-      .markup('<br>')
-      .type('to avoid ')
-      .pause(50)
-      .type('Failure')
-      .pause(300)
-      .delete(7)
-      .pause(500)
-      .type('Error')
-      .pause(250)
-      .delete(5)
-      .pause(1500)
-      .type('Failure+Error')
-      .replace(13, 'FailError', 'highlight')
+      .pauseFor(2500)
+      .typeString('Following the <br>"Logical Sequential Steps" <br>to avoid ')
+      .pauseFor(50)
+      .typeString(`Failure`)
+      .pauseFor(300)
+      .deleteChars(7)
+      .pauseFor(500)
+      .typeString(`Error`)
+      .pauseFor(250)
+      .deleteChars(5)
+      .pauseFor(1500)
+      .typeString(`<span class="highlight">Failure+Error</span>`)
+      .callFunction(() => {
+        let elem = textElem.querySelector('.highlight');
+        elem.textContent = 'FailError';
+        elem.id = 'highlight';
+      })
       .start();
   }
 }
